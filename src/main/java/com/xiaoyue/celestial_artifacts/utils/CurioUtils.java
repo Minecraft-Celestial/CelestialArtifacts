ca9c92bb2232faa0bb45a1af8202d869c0a2ba82
lcy0x1 <chengyilin@umail.ucsb.edu>
2024-04-30 16:42:22 +0800

    bug fix

--- src/main/java/com/xiaoyue/celestial_artifacts/utils/CurioUtils.java
+++ src/main/java/com/xiaoyue/celestial_artifacts/utils/CurioUtils.java
@@ -2,6 +2,7 @@ package com.xiaoyue.celestial_artifacts.utils;
 
 import com.xiaoyue.celestial_artifacts.content.core.modular.CurioCacheCap;
 import com.xiaoyue.celestial_artifacts.content.curios.curse.CatastropheScroll;
+import com.xiaoyue.celestial_artifacts.register.CAItems;
 import dev.xkmc.l2library.capability.conditionals.ConditionalData;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.Item;
@@ -14,7 +15,8 @@ public class CurioUtils {
 	}
 
 	public static boolean isCsOn(Player player) {
-		return ConditionalData.HOLDER.get(player).hasData(CatastropheScroll.TOKEN.getKey());
+		return ConditionalData.HOLDER.get(player).hasData(CatastropheScroll.TOKEN.getKey()) ||
+				hasCurio(player, CAItems.CATASTROPHE_SCROLL.get());
 	}
 
 	public static int getCurseAmount(Player player) {
